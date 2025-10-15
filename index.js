1cdda09038c9fe484651b7919e8c4f4ca2bf40b9
byeong <qudcks4885@gmail.com>
2025-10-15 21:24:32 +0900

    feat: then이 promise 리턴하도록 수정 및 콜백함수 비동기 실행
    - flattening X

--- index.js
+++ index.js
@@ -1,46 +1,99 @@
 class MockPromise {
-  constructor(callback) {
+  constructor(executor) {
     this.state = "pending";
     this.value;
     this.reason;
-    callback(this._resolve.bind(this), this._reject.bind(this));
+    this.callbackQueue = [];
+
+    try {
+      executor(this._resolve.bind(this), this._reject.bind(this));
+    } catch (error) {
+      this._reject(error);
+    }
   }
 
-  _resolve(value, reason) {
+  _resolve(value) {
     if (this.state !== "pending") return;
     this.value = value;
-    this.reason = reason;
     this.state = "fulfilled";
+
+    // queue 작업 실행
+    this._onStateChange();
   }
 
-  _reject(value, reason) {
+  _reject(reason) {
     if (this.state !== "pending") return;
-    this.value = value;
     this.reason = reason;
     this.state = "rejected";
+
+    //
+    this._onStateChange();
   }
 
-  then(cb) {
-    if (this.state === "fulfilled") {
-      // return this(cb);
-      // return new MockPromise(cb); -> then의 콜백은 인자로 resolve, reject를 가지는 구조가 아니다.
-
-      // cb의 리턴값을 다음 프로미스(새로운 promise?)의 콜백 인자로 넘겨줌
-      if (typeof cb === "function") {
-        this.value = cb(this.value);
-      }
-
-      return this;
+  _onStateChange() {
+    const cb = this.callbackQueue.shift();
+    if (cb) {
+      cb(this.state, this.value ?? this.reason);
     }
-    return this;
+  }
+
+  then(onFulfilled, onRejected) {
+    if (this.state === "pending") {
+      const promise = new MockPromise(() => console.log("mock promise"));
+
+      const executor = (promise) => (state, arg) => {
+        queueMicrotask(() => {
+          try {
+            if (state === "fulfilled") {
+              promise._resolve(onFulfilled(arg));
+            } else {
+              promise._resolve(onRejected(arg));
+            }
+          } catch (error) {
+            promise._reject(error);
+          }
+        });
+      };
+
+      this.callbackQueue.push(executor(promise));
+
+      return promise;
+    } else {
+      return new MockPromise((resolve, reject) => {
+        queueMicrotask(() => {
+          try {
+            const result =
+              this.state === "fulfilled"
+                ? onFulfilled(this.value)
+                : onRejected(this.reason);
+            resolve(result);
+          } catch (error) {
+            reject(error);
+          }
+        });
+      });
+    }
   }
 }
 
 function main() {
   // step - 1
+  console.log("step - 1");
   const p = new MockPromise((resolve) => resolve(1));
   p.then((v) => v * 10)
     .then((v) => v + 10)
+    .then((v) => console.log(v)); // 20
+
+  console.log("step - 2");
+
+  // step - 2
+  function later(v, t = 50) {
+    return new MockPromise((res) => setTimeout(() => res(v), t));
+  } // later: 50ms 뒤 fulfilled되는 프로미스 반환.
+
+  new MockPromise((res) => res(1))
+    .then((v) => later(v * 2))
+    .then((v) => later(v + 3)) // flattening -> promise 속성을 자세히 모르는 듯.
     .then((v) => console.log(v));
 }
 
